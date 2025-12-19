ff7d0230b5b84699c4d9966a0da6c482b523b978
nvhung2711 <nvhung2711@gmail.com>
2025-12-19 11:44:21 +1100

    Finish up the hero section and the search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import SearchForm from "../../components/SearchForm";
 
-const Page = () => {
+const Home = async ({ searchParams }: {
+    searchParams: Promise<{ query?: string }>
+}) => {
+    const query = (await searchParams).query;
+
     return (
         <>
-            <h1 className="text-2xl">Home</h1>
+            <section className="pink_container">
+                <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+
+                <p className="sub-heading max-w-3xl!">
+                    Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+                </p>
+
+                <SearchForm query={query} />
+            </section>
         </>
     );
 };
 
-export default Page;
+export default Home;
